6bcbca78e039d4aaf9b9a68f487f5ed2c63f954d
Aditya <adityateja0118@gmail.com>
2026-07-20 13:28:53 +0530

    Initial set uo

---
+++ src/main/java/com/AccountReceivableManagement/AccountReceivableManagementApplication.java
@@ -0,0 +1,13 @@
+package com.AccountReceivableManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountReceivableManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountReceivableManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AccountReceivableManagement/AccountReceivableManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccountReceivableManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountReceivableManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
