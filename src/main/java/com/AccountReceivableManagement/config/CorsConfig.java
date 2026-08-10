23752068676e64ecd98a99574c99d4bbf29bdc75
Aditya <adityateja0118@gmail.com>
2026-08-10 11:56:20 +0530

    CORS

--- src/main/java/com/AccountReceivableManagement/config/CorsConfig.java
+++ src/main/java/com/AccountReceivableManagement/config/CorsConfig.java
@@ -1,15 +1,17 @@
 package com.AccountReceivableManagement.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("*")
+//                .allowedOrigins("*")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
